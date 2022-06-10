db00d495e50c58c895215bb68a2f22442b8ab332
luciebottin <lucie.bottin@efrei.net>
2022-06-10 09:48:39 +0200

    actions

---
+++ api.js
@@ -0,0 +1,32 @@
+// api.js
+
+const express = require('express')
+const app = express();
+const port = 8081;
+
+app.post('/predict', function (req, res) {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    //const query = "https://api.openweathermap.org/data/2.5/weather?lat&lon=$LONG&appid=$API_KEY"
+    res.send({
+      'lat': lat,
+      'lon': lon,
+    });
+  });
+    res.status(200).send(query);
+  });
+
+  app.get('/api', function(req, res) {
+    const user_id = req.query.id;
+    const token = req.query.token;
+    const geo = req.query.geo;
+  
+    res.send({
+      'user_id': user_id,
+      'token': token,
+      'geo': geo
+    });
+  });
+
+console.log(`==> API listening on port ${port}`)
+app.listen(port);
--- file.sh
+++ file.sh
@@ -3,3 +3,4 @@
 
 curl "https://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LONG&appid=$API_KEY"
 
+exec nodejs api.js
